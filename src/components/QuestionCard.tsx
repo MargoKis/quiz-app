db92be29dda085b7efb34b5c25e2ee3857bb0a0b
Marharyta <margaritakislajalemon@gmail.com>
2024-01-17 16:25:01 +0200

    update App, QuestionCard, API, utils

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+// Types
+import { AnswerObject } from "../App";
 
 type Props = {
   question: string;
   answers: string[];
-  callback: any;
-  userAnswer: any;
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNr: number;
   totalQuestions: number;
 };
@@ -17,21 +19,25 @@ const QuestionCard: React.FC<Props> = ({
   questionNr,
   totalQuestions,
 }) => {
-  return <div>
-    <p className="number">
-        Question: {questionNr} / {totalQuestions}
-    </p>
-    <p dangerouslySetInnerHTML={{__html: question}} />
+  return (
     <div>
-        {answers.map(answer => (
-            <div>
-                <button disabled={userAnswer} onClick={callback}>
-                    <span dangerouslySetInnerHTML={{__html: answer }} />
-                </button>
-            </div>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer) => (
+          <button
+            disabled={userAnswer ? true : false}
+            value={answer}
+            onClick={callback}
+          >
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
         ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default QuestionCard;
